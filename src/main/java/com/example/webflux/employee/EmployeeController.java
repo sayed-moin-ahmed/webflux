d5481ffb987063a95dfbe7b90a1ee849394f96bf
sayed-moin-ahmed <sayed.moin.85@gmail.com>
2021-05-06 13:23:22 +0530

    Update EmployeeController.java

--- src/main/java/com/example/webflux/employee/EmployeeController.java
+++ src/main/java/com/example/webflux/employee/EmployeeController.java
@@ -1,12 +1,52 @@
 package com.example.webflux.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class EmployeeController {
-
     @Autowired
-    private  EmployeeService employeeService;
+    private EmployeeService employeeService;
+
+    @RequestMapping(value = { "/create", "/" }, method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Employee e) {
+        employeeService.create(e);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Mono<Employee>> findById(@PathVariable("id") Integer id) {
+        Mono<Employee> e = employeeService.findById(id);
+        HttpStatus status = e != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        return new ResponseEntity<Mono<Employee>>(e, status);
+    }
+
+    @RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
+    public Flux<Employee> findByName(@PathVariable("name") String name) {
+        return employeeService.findByName(name);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Employee> findAll() {
+        Flux<Employee> emps = employeeService.findAll();
+        return emps;
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Employee> update(@RequestBody Employee e) {
+        return employeeService.update(e);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("id") Integer id) {
+        employeeService.delete(id).subscribe();
+    }
 
 }
