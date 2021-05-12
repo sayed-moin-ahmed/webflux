bf88cceeaf549eb5aac9a9aaa157ff4ac582e6b3
sayed-moin-ahmed <sayed.moin.85@gmail.com>
2021-05-12 13:32:11 +0530

    Update FlowableSample.java

--- src/main/java/com/example/webflux/rxjava/FlowableSample.java
+++ src/main/java/com/example/webflux/rxjava/FlowableSample.java
@@ -1,10 +1,14 @@
 package com.example.webflux.rxjava;
 
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Flow;
+import java.util.stream.Collectors;
 
 public class FlowableSample {
     public static void main(String[] args){
@@ -14,5 +18,15 @@ public class FlowableSample {
                 .map(v -> v*v)
                 .blockingSubscribe(squares::add);
         System.out.println(squares);
+
+        Single<List<Integer>> result = Flowable.range(1,10)
+                .filter(i -> i%2 == 0)
+                .collect(Collectors.toList());
+        System.out.println(result.blockingGet());
+
+        Flowable.range(1, 64)
+                .map(i -> {return Integer.toString(i);})
+                .forEach(System.out::println)
+                .dispose();
     }
 }
