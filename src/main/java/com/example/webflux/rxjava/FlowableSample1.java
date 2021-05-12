58f795169ce86baa8cd1b60ab8ca5b6334a361eb
sayed-moin-ahmed <sayed.moin.85@gmail.com>
2021-05-12 19:42:32 +0530

    Update FlowableSample1.java

--- src/main/java/com/example/webflux/rxjava/FlowableSample1.java
+++ src/main/java/com/example/webflux/rxjava/FlowableSample1.java
@@ -1,2 +1,11 @@
-package com.example.webflux.rxjava;public class FlowableSample1 {
+package com.example.webflux.rxjava;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class FlowableSample1 {
+    public static void main(String[] args){
+        Flowable.fromArray(1,2,3,4,5).subscribe(System.out::println);
+        Flowable.just("a","b","c").observeOn(Schedulers.computation()).subscribe(System.out::println);
+    }
 }
